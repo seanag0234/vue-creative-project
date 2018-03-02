c833be5e6c82c703b3ee879b83d7382be3390b2a
Sean_George <seangeorge0@gmail.com>
2018-03-01 18:00:20 -0700

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <nav-bar></nav-bar>
     <router-view/>
+    <vue-footer></vue-footer>
   </div>
 </template>
 
 <script>
   import NavBar from './components/NavBar'
+  import VueFooter from './components/Footer'
   export default {
     name: 'App',
-    components:{NavBar}
+    components:{NavBar, VueFooter}
   }
 </script>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer">
+      <div class="columns">
+        <div class="column is-offset-one-quarter is-half">
+          <a class="button is-info" href="https://github.com/seanag0234/vue-creative-project" target="_blank"><i style="padding-right: 5px" class="fa fa-github" aria-hidden="true"></i> Github</a>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+  .footer {
+    position:fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 1rem;
+    /*background-color: inherit;*/
+    text-align: center;
+  }
+
+</style>
